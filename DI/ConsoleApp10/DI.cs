5f7b84d0840aa54a0e397af97d9d5b7f7de7bcd4
ScrapperBoo <96202597+ScrapperBoo@users.noreply.github.com>
2021-12-26 18:36:49 +0300

    task4

---
+++ DI/ConsoleApp10/DI.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace DI
+{
+    public class DI
+    {
+        private List<SD> dependencies;
+        public DI()
+        {
+            dependencies = new List<SD>();
+        }
+        public void AddUnstable<TService, TImplementation>()
+        {
+            dependencies.Add(new SD(typeof(TService), typeof(TImplementation), ST.Transient));
+        }
+        public void AddSingleton<TService, TImplementation>()
+        {
+            dependencies.Add(new SD(typeof(TService), typeof(TImplementation), ST.Singleton));
+        }
+        public T Get<T>() => (T)Get(typeof(T));
+        public object Get(Type sT)
+        {
+            var specifier = dependencies.SingleOrDefault(x => x.ServiceType == sT);
+            if (specifier == null)
+            {
+                throw new Exception("not found");
+            }
+            if (specifier.Implementation != null)
+            {
+                return specifier.Implementation;
+            }
+            var relevantType = specifier.ImplementationType;
+            var constInfo = relevantType.GetConstructors().First();
+            if (constInfo.GetParameters().Any(x => chekCycle(sT, x.ParameterType)))
+            {
+                throw new Exception("Цикл");
+            }
+            var parameters = constInfo.GetParameters().Select(x => Get(x.ParameterType)).ToArray();
+            var implementation = Activator.CreateInstance(relevantType, parameters);
+            if (specifier.LifeTime == ST.Singleton)
+            {
+                specifier.Implementation = implementation;
+            }
+            return implementation;
+        }
+        public bool chekCycle(Type serviceType, Type parametrType)
+        {
+            var descriptor = dependencies.SingleOrDefault(x => x.ServiceType == parametrType);
+            var actualType = descriptor.ImplementationType;
+            var constructorType = actualType.GetConstructors().First();
+            return constructorType.GetParameters().Any(x => Equals(serviceType, x.ParameterType));
+        }
+    }
+}
---
+++ DI/ConsoleApp10/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace DI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var container = new DI();
+            container.AddUnstable<iA, A>();
+            container.AddUnstable<iB, B>();
+            container.Get<iB>();
+        }
+    }
+}
